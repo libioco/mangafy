11218f5a5a53f31a1a08f5e6fdaccf8a60f03237
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-08-17 15:17:20 -0400

    Create login and register pages. Add home, profile, and cart.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,59 @@ import {
     Outlet
 } from 'react-router-dom';
 import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Home.jsx';
+import Cart from './pages/Cart.jsx';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
 import './styles.scss';
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/profile",
+                element: <Profile/>
+            },
+            {
+                path: "/cart",
+                element: <Cart/>
+            }
+        ]
+    }, 
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/register",
+        element: <Register/>
+    },
+]);
 
 function App() {
 	return (
 		<div className="app">
             <div className="container">
-                <Login/>
+                <RouterProvider router={router}/>
             </div>
         </div>
 	);
